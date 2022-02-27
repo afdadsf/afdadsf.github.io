319ebfdd8b01a5fd2fa36a817f1f41dda6cec603
afdadsf <100248992+afdadsf@users.noreply.github.com>
2022-02-28 01:16:47 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,5 @@
 targetImage.addEventListener("click", function() {
+    alert("点击了图片");
     let targetImage = document.querySelector("#smart-image");
     targetImage.classList.remove("small");
 });
